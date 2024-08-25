1c28d21bbe4b083f6da9e077b46322168c13ffef
Atter12xd <1485275@senati.pe>
2024-08-25 01:37:18 -0500

    arrays con un rango de valores

--- numpy/python_numpy.py
+++ numpy/python_numpy.py
@@ -8,6 +8,10 @@ zeros = np.zeros((2, 3)) # 2x3 matrix of zeros
 
 print (zeros)
 
-ones = np.ones((3, 2)) #3xx2 matrix of ones 
+ones = np.ones((2, 3)) #3xx2 matrix of ones 
 
 print (ones)
+
+range_arr = np.arange(0, 10, 2) # array [0, 2, 4, 6, 8]
+
+print (range_arr)
