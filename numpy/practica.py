a320ae906221727822561ba3f7160b53ee489c58
Atter12xd <1485275@senati.pe>
2024-08-25 01:48:40 -0500

    indexecion y slicing

--- numpy/practica.py
+++ numpy/practica.py
@@ -7,3 +7,8 @@ arr_1d = np.array([1, 2, 3])
 arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
 result = arr_1d + arr_2d
 print(result)
+
+
+arr = np.array([10, 20, 30, 40, 50])
+sliced = arr[1:4]  # Obtiene [20, 30, 40]
+print(sliced)
