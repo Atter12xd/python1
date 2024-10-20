27a2fe9457dfebb83bd71d357c401ec9b782b84f
Atter12xd <1485275@senati.pe>
2024-10-20 17:17:01 -0500

    cargar el conjunto de datos

--- Machine_Learning/ML.py
+++ Machine_Learning/ML.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+
+# Datos ficticios de precios de casas
+data = {
+    'Size': [850, 900, 1200, 1500, 1800, 2200, 2500, 2800, 3000, 3300],
+    'Price': [95, 110, 130, 160, 185, 220, 250, 270, 300, 330]
+}
+
+df = pd.DataFrame(data)
+
+# Mostrar los primeros 5 registros
+print(df.head())
