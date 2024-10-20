32d63f6218d4a2ee0a31e0145134df4a89572c4f
Atter12xd <1485275@senati.pe>
2024-10-20 17:20:35 -0500

    Hacer predicciones

--- Machine_Learning/ML.py
+++ Machine_Learning/ML.py
@@ -29,3 +29,9 @@ y = df['Price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Crear el modelo de regresión lineal
+model = LinearRegression()
+
+# Entrenar el modelo
+model.fit(X_train, y_train)
+
